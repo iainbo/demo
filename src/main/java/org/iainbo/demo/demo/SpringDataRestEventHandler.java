66be965fef4fbcc477dc31fdcdf4cb08d24d3881
iainbo <iain.bogucki@sky.com>
2017-11-07 11:46:53 +0000

    Fixed bug where page would not update when user details were updated.

--- src/main/java/org/iainbo/demo/demo/SpringDataRestEventHandler.java
+++ src/main/java/org/iainbo/demo/demo/SpringDataRestEventHandler.java
@@ -5,6 +5,7 @@ import org.iainbo.demo.demo.entities.Manager;
 import org.iainbo.demo.demo.repositories.ManagerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
+import org.springframework.data.rest.core.annotation.HandleBeforeSave;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -25,11 +26,25 @@ public class SpringDataRestEventHandler {
         String name = SecurityContextHolder.getContext().getAuthentication().getName();
         Manager manager = this.managerRepository.findByName(name);
         if(manager == null){
-            Manager newManager = new Manager();
-            newManager.setName(name);
-            newManager.setRoles(new String[]{"ROLE_MANAGER"});
-            manager=this.managerRepository.save(newManager);
+            manager = createNewManager(name);
         }
         employee.setManager(manager);
+    }
+
+    @HandleBeforeSave
+    public void applyUserInformationBeforeSave(Employee employee){
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        Manager manager = this.managerRepository.findByName(name);
+        if(manager == null){
+            manager = createNewManager(name);
+        }
+        employee.setManager(manager);
+    }
+
+    private Manager createNewManager(String name){
+        Manager newManager = new Manager();
+        newManager.setName(name);
+        newManager.setRoles(new String[]{"ROLE_MANAGER"});
+        return this.managerRepository.save(newManager);
     }
 }
