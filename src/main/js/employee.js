54540aac2db92e95daa2764d3a4ea693f0308edf
iainbo <iain.bogucki@sky.com>
2017-12-07 14:18:41 +0000

    Removed updateDialog.js, added modal to employee

--- src/main/js/employee.js
+++ src/main/js/employee.js
@@ -3,11 +3,12 @@
 import DropDownButton from 'react-bootstrap/lib/DropdownButton';
 import MenuItem from 'react-bootstrap/lib/MenuItem';
 import Modal from 'react-bootstrap/lib/Modal';
+import Button from 'react-bootstrap/lib/Button';
+import Form from 'react-bootstrap/lib/Form';
 import style from 'bootstrap/dist/css/bootstrap.css';
 
-import {UpdateDialog} from './updateDialog';
-
 const React = require('react');
+const ReactDOM = require('react-dom');
 
 export class Employee extends React.Component {
 
@@ -16,6 +17,7 @@ export class Employee extends React.Component {
 
         this.state = {showModal: false};
 
+        this.handleSubmit = this.handleSubmit.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
         this.update = this.update.bind(this);
         this.close = this.close.bind(this);
@@ -33,7 +35,26 @@ export class Employee extends React.Component {
         this.setState({showModal: false});
     }
 
+    handleSubmit(e) {
+        e.preventDefault();
+        var updatedEmployee = {};
+        this.props.attributes.forEach(attribute => {
+            updatedEmployee[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
+        });
+        this.props.onUpdate(this.props.employee, updatedEmployee);
+        this.close();
+    }
+
     render() {
+
+        var inputs = this.props.attributes.map(attribute =>
+            <p key={this.props.employee.entity[attribute]}>
+                <input type="text" placeholder={attribute}
+                       defaultValue={this.props.employee.entity[attribute]}
+                       ref={attribute} className="field" />
+            </p>
+        );
+
         return (
             <tr>
                 <td>{this.props.employee.entity.firstName}</td>
@@ -49,14 +70,21 @@ export class Employee extends React.Component {
                     </DropDownButton>
                 </td>
 
+
                     <Modal show={this.state.showModal} onHide={this.close}>
                         <Modal.Header>
                             <Modal.Title>Update Employee</Modal.Title>
                         </Modal.Header>
                         <Modal.Body>
-                            <UpdateDialog employee={this.props.employee}
-                                          attributes={this.props.attributes}
-                                          onUpdate={this.props.onUpdate}/>
+                            <div>
+                                <h2>Update an employee</h2>
+
+                                <Form>
+                                    {inputs}
+                                </Form>
+                            </div>
+                            <Button onClick={this.handleSubmit}>Update</Button>
+                            <Button onClick={this.close}>Cancel</Button>
                         </Modal.Body>
                     </Modal>
 
